2d4bb86f40558733196877a4536e0383e0d9ba72
omerfsen <omerfsen@gmail.com>
2020-04-21 13:34:49 +0100

    Convert to Terraform 0.12

--- environments/prod/vars.tf
+++ environments/prod/vars.tf
@@ -1,4 +1,12 @@
-variable "terraform_state_bucket" {}
-variable "terraform_state_bucket_region" {}
-variable "aws_default_region" {}
-variable "org_name" {}
+variable "terraform_state_bucket" {
+}
+
+variable "terraform_state_bucket_region" {
+}
+
+variable "aws_default_region" {
+}
+
+variable "org_name" {
+}
+
--- master/organization/overrides/backend_local_override.tf
+++ master/organization/overrides/backend_local_override.tf
@@ -1,4 +1,6 @@
 // should only be used by init.sh for the initial setup
 terraform {
-  backend "local" {}
+  backend "local" {
+  }
 }
+
